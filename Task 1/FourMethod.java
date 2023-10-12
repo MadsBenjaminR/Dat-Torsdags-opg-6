540f74b8b60cd1452c978c2a117c35668cebd53b
Mads Benjamin Ribberholt <benjamin.ribberholt@gmail.com>
2023-10-12 10:54:30 +0200

    Task 1

---
+++ Task 1/FourMethod.java
@@ -0,0 +1,34 @@
+public class FourMethod {
+
+    public void MethodA(String input ){
+
+        if(input.equals("Adolf")){
+            System.out.print('j');
+        }else{
+            System.out.print('v');
+        }
+    }
+
+    public void MethodB(String start){
+        System.out.print(" er");
+    }
+
+    public void MethodC(String input){
+        if(input.equals("NO")){
+            System.out.print("t");
+        }else{
+            System.out.print("a");
+        }
+
+
+    }
+
+    public void MethodD(int number){
+        if(number < 10) {
+            System.out.print(" s");
+        }else {
+            System.out.print("o");
+        }
+
+    }
+}
---
+++ Task 1/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String [] args){
+
+        FourMethod four = new FourMethod();
+
+        four.MethodA("Adolf");
+        four.MethodC("");
+        four.MethodA("Whatever");
+        four.MethodC("");
+        four.MethodB("?");
+        four.MethodD(5);
+        four.MethodA("Adolf");
+        four.MethodD(11);
+        four.MethodA("Dølle");
+        four.MethodC("NO");
+
+
+    }
+}
